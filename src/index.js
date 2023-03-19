11662f248229c6060784fba71b7b1f58d664c34e
Adam Farrow <adamfarrow1@gmail.com>
2023-03-19 16:05:43 -0400

    changes the rest of the website to react

--- src/index.js
+++ src/index.js
@@ -8,17 +8,39 @@ import StudyRooms from './home/StudyRooms';
 import Reviews from './home/Reviews';
 import Cta from './login_register/Cta';
 import Footer from './Footer';
+import Matching from './Study-Group/Matching';
+import { BrowserRouter, Routes , Route, Navigate} from 'react-router-dom';
+import About from './About-us/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Header />
-    <Offer></Offer>
-    <StudyRooms></StudyRooms>
-    <Reviews></Reviews>
-    <Cta></Cta>
-    <Footer></Footer>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Header />
+      <Routes>
+          <Route path='/' element={
+            <>
+              <Offer></Offer>
+              <StudyRooms></StudyRooms>
+              <Reviews></Reviews>
+            </>
+          }></Route>
+
+          <Route path='studygroups' element={
+            <Matching></Matching>
+          }></Route>
+
+          <Route path='about' element={
+            <About></About>
+          }></Route>
+
+          
+
+        </Routes>
+      <Cta></Cta>
+      <Footer></Footer>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
